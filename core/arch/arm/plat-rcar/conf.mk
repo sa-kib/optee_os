2abfad12b7512ae4b3424c2f77a5a10e9a13c4ee
Sergiy Kibrik <Sergiy_Kibrik@epam.com>
2021-05-17 06:24:38 +0000

    plat: rcar: enable hardware RNG support
    Enable use of hw_get_random_byte() and RNG PTA.
    
    Signed-off-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>

--- core/arch/arm/plat-rcar/conf.mk
+++ core/arch/arm/plat-rcar/conf.mk
@@ -7,6 +7,7 @@ $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 $(call force,CFG_SCIF,y)
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
 $(call force,CFG_CORE_ARM64_PA_BITS,36)
+$(call force,CFG_WITH_SOFTWARE_PRNG,n)
 $(call force,CFG_TEE_CORE_NB_CORE,8)
 $(call force,CFG_ARM64_core,y)
 $(call force,CFG_WITH_LPAE,y)
@@ -14,6 +15,7 @@ $(call force,CFG_WITH_LPAE,y)
 CFG_TZDRAM_START ?= 0x44100000
 CFG_TZDRAM_SIZE	 ?= 0x03D00000
 CFG_TEE_RAM_VA_SIZE ?= 0x100000
+CFG_RNG_QUALITY ?= 1024
 supported-ta-targets = ta_arm64
 
 CFG_DT ?= y
